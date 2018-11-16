910bc26287de7477925ed8f9b133e82319c7841b
Kernael92 <kernaelapuko5@gmail.com>
2018-11-17 01:18:41 +0300

    create a category class that will define all the pitch categories

--- app/models.py
+++ app/models.py
@@ -3,9 +3,18 @@ from . import db
 class PItch(db.Model):
     __tablename__ = 'pitches'
     id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.string(255))
-    description = db.Column(db.string)
+    name = db.Column(db.String(255))
+    description = db.Column(db.String(1000))
 
 
     def __repr__(self):
         return f'Pitch {self.description}'
+
+class Category(db.Model):
+    __tablename = 'categories'
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
+
+    def __repr__(self):
+        return f'Pitch {self.name}'
