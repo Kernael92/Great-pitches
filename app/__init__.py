26bb9283a8e7331985681f3ee22e04c5ae5bf4cd
Kernael92 <kernaelapuko5@gmail.com>
2018-11-16 23:48:28 +0300

    define the SQLACHEMY DATABASE URI and initialize in our app

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask 
 from flask_bootstrap import Bootstrap
 from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -16,5 +19,6 @@ def create_app(config_name):
 
     # Initializing Flask Extensions
     bootstrap.init_app(app)
+    db.init_app(app)
 
     return app
