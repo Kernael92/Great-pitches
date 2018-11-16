04690b2970bce48b39f5c003a315cce8eb9bc894
Kernael92 <kernaelapuko5@gmail.com>
2018-11-17 01:09:04 +0300

    initialize the pitch class objects and add python shell inside our application

--- manage.py
+++ manage.py
@@ -1,5 +1,7 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import PItch
+
 
 
 # Creating app instance 
@@ -15,6 +17,9 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,Pitch = PItch)
 
 
 if __name__ == '__main__':
