12776b331c0aab93342f1b5f9a38998ac0361512
Kernael92 <kernaelapuko5@gmail.com>
2018-11-17 16:18:29 +0300

    add macros to display category list

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,18 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-
+from .. import db
+from ..models import Pitch 
 @main.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
     '''
     title = 'Home- welcome to the best pitches website online'
-    return render_template('index.html', titlt = title)
+    salespitch = Pitch.query.filter_by(category = "Sales")
+    investorpitch = Pitch.query.filtet_by(category = "Investor")
+    employeespitch = Pitch.query.filter_by(category = "Employees")
+    Pickuplinepitch = Pitch.query.filter_by(category = "Pickup")
+
+    return render_template('index.html', titlt = title,salespitch = salespitch, investorpitch = investorpitch, employeespitch = employeespitch, pickuplinepitch = Pickuplinepitch)
 
 
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager,Server
-from app.models import PItch
+from app.models import Pitch
 from flask_migrate import Migrate, MigrateCommand
 
 
@@ -23,7 +23,7 @@ def test():
     unittest.TextTestRunner(verbosity=2).run(tests)
 @manager.shell
 def make_shell_context():
-    return dict(app = app, db = db, Pitch = PItch )
+    return dict(app = app, db = db, Pitch = Pitch )
 
 
 if __name__ == '__main__':
