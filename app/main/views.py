bf49423cf0a68a067e2ade2cc291fc3f48d39f11
Kernael92 <kernaelapuko5@gmail.com>
2018-11-17 23:57:30 +0300

    define the new comment view function and update macros

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 from flask import render_template,request,redirect,url_for
 from . import main
 from .. import db
-from ..models import Pitch 
+from ..models import Pitch,Comment 
+from .forms import commentForm
 @main.route('/', methods = ['GET','POST'])
 def index():
     '''
@@ -15,5 +16,20 @@ def index():
     title = 'Home- welcome to the best pitches website online'
 
     return render_template('index.html', title = title,pitch =pitch, Sales= Sales, Investor = Investor, Employees = Employees, Pickup = Pickup)
+
+@main.route('/comment/new/<int: pitch_id>', methods = ['GET', 'POST'])
+def new_comment(pitch_id):
+    form = commentForm()
+    pitch=Pitch.query.get(pitch_id)
+    if form.validate_on_submit():
+        description = form.description.data
+        new_comment = Comment(description = description, pitch_id = pitch_id)
+        db.session.add(new_comment)
+        db.session.commit()
+
+        return redirect(url_for('.new_comment', pitch_id = pitch_id))
+    
+    all_comments = Comment.query.filter_by(pitch_id=pitch_id).all()
+    return render_template('comments.html',form = form ,comment = all_comments, pitch=pitch)
 
 
