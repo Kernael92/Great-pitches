7da4ed38d6587995bb923771c17a9ae48b121dbd
Kernael92 <kernaelapuko5@gmail.com>
2018-11-17 17:45:27 +0300

    add navigation bar

--- app/main/views.py
+++ app/main/views.py
@@ -2,17 +2,18 @@ from flask import render_template,request,redirect,url_for
 from . import main
 from .. import db
 from ..models import Pitch 
-@main.route('/')
+@main.route('/', methods = ['GET','POST'])
 def index():
     '''
     View root page function that returns the index page and its data
     '''
+    pitch = Pitch.query.filter_by().first()
+    Sales = Pitch.query.filter_by(category = "Sales")
+    Investor = Pitch.query.filter_by(category = "Investor")
+    Employees = Pitch.query.filter_by(category = "Employees")
+    Pickup = Pitch.query.filter_by(category = "Pickup")
     title = 'Home- welcome to the best pitches website online'
-    salespitch = Pitch.query.filter_by(category = "Sales")
-    investorpitch = Pitch.query.filtet_by(category = "Investor")
-    employeespitch = Pitch.query.filter_by(category = "Employees")
-    Pickuplinepitch = Pitch.query.filter_by(category = "Pickup")
 
-    return render_template('index.html', titlt = title,salespitch = salespitch, investorpitch = investorpitch, employeespitch = employeespitch, pickuplinepitch = Pickuplinepitch)
+    return render_template('index.html', titlt = title,pitch =pitch, Sales= Sales, Investor = Investor, Employees = Employees, Pickup = Pickup)
 
 
