bd3a30f12a93699cae01802909ec272909d60c5a
Kernael92 <kernaelapuko5@gmail.com>
2018-11-19 16:41:08 +0300

    send an email to a new user when they sign up

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
 photos = UploadSet('photo',IMAGES)
-mail = Mail
+mail = Mail()
 
 def create_app(config_name):
     app = Flask(__name__)
--- app/auth/views.py
+++ app/auth/views.py
@@ -4,6 +4,8 @@ from ..models import User
 from .forms import RegistrationForm,LoginForm
 from .. import db
 from . import auth
+from ..email import mail_message
+
 
 
 @auth.route('/login',methods = ['GET','POST'])
@@ -28,6 +30,9 @@ def register():
         user = User(email = form.email.data, username = form.username.data, password = form.password.data)
         db.session.add(user)
         db.session.commit()
+
+        mail_message("Welcome to best pitches website online","email/welcome_user",user.email,user=user)
+
         return redirect(url_for('auth.login'))
     title = 'New Account'
     return render_template('auth/register.html',title=title,RegistrationForm = form)
@@ -36,4 +41,5 @@ def register():
 def logout():
     logout_user()
     return redirect(url_for("main.index"))
+
 
